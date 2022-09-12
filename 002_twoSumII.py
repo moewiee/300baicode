74c0e329db70a13c5167265fc05606a232098939
Your Name <you@example.com>
2022-09-12 16:56:32 +0700

    update with self imports

--- 002_twoSumII.py
+++ 002_twoSumII.py
@@ -1,35 +1,20 @@
-def binary_search(arr, low, high, x):
- 
-    # Check base case
-    if high >= low:
- 
-        mid = (high + low) // 2
- 
-        # If element is present at the middle itself
-        if arr[mid] == x:
-            return mid
- 
-        # If element is smaller than mid, then it can only
-        # be present in left subarray
-        elif arr[mid] > x:
-            return binary_search(arr, low, mid - 1, x)
- 
-        # Else the element can only be present in right subarray
-        else:
-            return binary_search(arr, mid + 1, high, x)
- 
-    else:
-        # Element is not present in the array
-        return -1
-    
+binary_search = __import__('019_binarySearch')
+
 class Solution:
-    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+    def __init__(self):
+        self.binary_searcher = binary_search.Solution()
+    
+    def twoSum(self, numbers: list, target: int) -> list:
         for i, n in enumerate(numbers):
             remainer = target - n
-            remainer_index = binary_search(numbers, 0, len(numbers) - 1, remainer)
+            remainer_index = self.binary_searcher.search(numbers, remainer)
             if remainer_index != -1:
                 if remainer_index == i:
                     return [i + 1, remainer_index + 2]
                 else:
                     return [i + 1, remainer_index + 1]
         
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.twoSum([2,7,11,15], 9))        
